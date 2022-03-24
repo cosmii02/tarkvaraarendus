65b9a83234b3a94ebb3edf39e8d6aa610739d089
Archanums <marcello.shirikov@gmail.com>
2022-03-24 13:28:54 +0200

    parandatud

--- lumememm.py
+++ lumememm.py
@@ -5,15 +5,13 @@ screen=pygame.display.set_mode([300,300])
 pygame.display.set_caption("Lumemees")
 
 screen.fill([0, 0, 0])
-pygame.draw.circle(screen, [255, 255, 255], [150,150], 25, 100)
-pygame.draw.circle(screen, [255, 255, 255], [150,110], 20, 100)
-pygame.draw.circle(screen, [255, 255, 255], [150,200], 30, 100)
-pygame.draw.polygon(screen, [255, 121, 0], [[147,115],[151,115],[150,120]], 2)
-pygame.draw.circle(screen, [0, 0, 0], [154,110], 3, 3)
-pygame.draw.circle(screen, [0, 0, 0], [146,110], 3, 3)
+pygame.draw.circle(screen, [255, 255, 255], [150,70], 31, 100)
+pygame.draw.circle(screen, [255, 255, 255], [150,135], 39, 100)
+pygame.draw.circle(screen, [255, 255, 255], [150,220], 52, 100)
+pygame.draw.polygon(screen, [255, 121, 0], [[149,90],[144,75],[154,75]], 5)
+pygame.draw.circle(screen, [0, 0, 0], [158,65], 4.5, 3)
+pygame.draw.circle(screen, [0, 0, 0], [140,65], 4.5, 3)
 pygame.display.flip()
-
-
 
 
 
