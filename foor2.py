f147b63aa7619cc736d485bb9a9c09d7f8ee10cf
Cosmii02 <tuisk2005@gmail.com>
2022-03-29 13:07:49 +0300

    Foor2 kommentaarid lisatud

--- foor2.py
+++ foor2.py
@@ -15,10 +15,10 @@ pygame.draw.circle(screen, [255, 255, 0], [150,150], 40, 100)
 # Joonistab rohelise ringi
 pygame.draw.circle(screen, [0, 255, 0], [150,235], 40, 100)
 
-pygame.draw.line(screen, [80,80,80], [150,285], [150,600], 9)
-pygame.draw.polygon(screen, [80, 80, 80], [[100,600],[200,600],[250,700],[50,700]], 2)
-pygame.draw.polygon(screen, [0, 0, 255], [[99,601],[201,601],[218,633],[81,633]], 0)
-pygame.draw.polygon(screen, [255, 255, 255], [[69,667],[233,667],[249,699],[51,699]], 0)
+pygame.draw.line(screen, [80,80,80], [150,285], [150,600], 9) # Joonistab foori posti
+pygame.draw.polygon(screen, [80, 80, 80], [[100,600],[200,600],[250,700],[50,700]], 2) # Joonistab aluse ääred
+pygame.draw.polygon(screen, [0, 0, 255], [[99,601],[201,601],[218,633],[81,633]], 0) # Joonistab ülesse sinise sisu
+pygame.draw.polygon(screen, [255, 255, 255], [[69,667],[233,667],[249,699],[51,699]], 0) # Joonistab alla valge sisu
 
 
 
