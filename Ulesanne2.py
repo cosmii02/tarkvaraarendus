c5618f9e89a03bcaf07e425c4187bcc588fba0f3
cosmii02 <applephone.kt@outlook.com>
2022-04-13 10:21:11 +0300

    Valmis lisaul2

--- Ulesanne2.py
+++ Ulesanne2.py
@@ -10,14 +10,19 @@ textb = pygame.image.load("img_2.png") # laeb pildi muutujale
 vikklogo = pygame.image.load("VIKK logo2.png") #laeb pildi muutujale
 vikklogo = pygame.transform.scale(vikklogo, (int(vikklogo.get_width()*0.75), int(vikklogo.get_height()*0.75)))
 cake= pygame.image.load("cake.png") # laeb pildi muutujale
-cake = pygame.transform.scale(cake, (int(cake.get_width() * 0.13), int(cake.get_height() * 0.13)))
-# Make img3 16% smaller
-textb = pygame.transform.scale(textb, (int(textb.get_width() * 0.85), int(textb.get_height() * 0.85)))
+cake = pygame.transform.scale(cake, (int(cake.get_width() * 0.13), int(cake.get_height() * 0.13))) # Teeb pildi 87% väiksemaks
+mook = pygame.image.load("mook.png") # laeb pildi muutujale
+mook = pygame.transform.scale(mook, (int(mook.get_width() * 0.20), int(mook.get_height() * 0.20))) #Teeb pildi 85% väiksemaks
+textb = pygame.transform.scale(textb, (int(textb.get_width() * 0.85), int(textb.get_height() * 0.85))) # Teeb pildi 15% väiksemaks
+# Rotate mook by 45 degrees
+mook = pygame.transform.rotate(mook, -45)
 screen.blit(bg, (0, 0)) # Lisab ekraani keskele pildi
 screen.blit(char, (104, 159)) # Lisab ekraanile karakteri pildi
 screen.blit(textb, (245, 66)) # Lisab musta tekstikasti pildi
 screen.blit(vikklogo, (0, 0)) # Lisab ekraanile vikki logo
 screen.blit(cake, (460, 190)) # Lisab ekraanile koogi pildi
+# Place mook right next to cake
+screen.blit(mook, (460 + cake.get_width() + -70, 100))
 font = pygame.font.SysFont("Arial", 20)
 text = font.render("Tere, olen Kenneth Tuisk", True, (255, 255, 255))
 screen.blit(text, (280, 140))
